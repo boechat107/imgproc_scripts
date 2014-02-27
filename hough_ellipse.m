b8de3a10b73f4d599028a8ebba8cc450259ad270
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-02-27 15:26:39 -0300

    Very inefficient implementation to detect ellipses
    * I could not wait for the result...

--- hough_ellipse.m
+++ hough_ellipse.m
@@ -11,12 +11,10 @@
 # ========================================================================================= 
 #
 # Using the following general equation to define an ellipse:
-# a(x - p)^2 + 2b(x - p)(y - q) + c(y - q)^2 + 1 = 0
-# with the restriction
-# ac - b^2 > 0 or c > b^2 / a
+# ax^2 + by^2 + cx + dy + e = 0
 
 
-function [] = hough_ellipse(img)
+function [a, b, c, d, e] = hough_ellipse(img)
 
     [width height] = size(img);
     %%% Defining the possible values for each variable of the ellipse equation.
@@ -24,22 +22,56 @@ function [] = hough_ellipse(img)
     %% vector with all not quantized values.
     vals_generator = @(fn, v) unique(fn(v));
     %% Function to quantize a value by a factor.
-    quantization = @(x, fac) floor(x/fac)*fac;
-    %% Quantization for p and q.
-    quant_pq = @(x) quantization(x, 2);
-    p_vals = vals_generator(quant_pq, [-width:width]);
-    q_vals = vals_generator(quant_pq, [-height:height]);
-    %% Quantization for a, b and c
-    quant_abc = @(x) quantization(x, 4);
+    qfac = 16;
+    quantization = @(x) floor(x/qfac)*qfac;
     max_val = max([width height]);
-    abc_vals = vals_generator(quant_abc, [-max_val:max_val]);
+    var_vals = vals_generator(quantization, [-max_val:max_val]);
 
-    abc_len = length(abc_vals);
-    acc = zeros(length(p), length(q), abc_len, abc_len, abc_len);
+    vars_len = length(var_vals);
+    acc = uint32(zeros(vars_len, vars_len, vars_len, vars_len, vars_len));
     %% Loop over all nonzero pixels of the image.
     [xs ys] = find(img);
-    for x = xs
-        for y = ys
-            
+    var_idxs = 1:vars_len;
+    for xyidx = 1:length(xs)
+        x = xs(xyidx);
+        y = ys(xyidx);
+        for aidx = var_idxs
+            for bidx = var_idxs
+                for cidx = var_idxs
+                    for didx = var_idxs
+                        a = var_vals(aidx);
+                        b = var_vals(bidx);
+                        c = var_vals(cidx);
+                        d = var_vals(didx);
+                        e = -(a*x^2 + b*y^2 + c*x + d*y);
+                        e_quant = quantization(e);
+                        %% Calculating the array index of the value e_quant.
+                        %% Otherwise, we would need to linearly search for the
+                        %% index in the array [-max_val:max_val].
+                        zero_idx = ceil(vars_len/2);
+                        rel_idx = e_quant/qfac;
+                        if e_quant > 0
+                            eidx = zero_idx + rel_idx;
+                        else 
+                            eidx = zero_idx - rel_idx;
+                        end 
+                        %% Incrementing the score of the corresponding ellipse
+                        %% variables.
+                        if eidx <= vars_len
+                            acc(aidx, bidx, cidx, didx, eidx)++;
+                        end
+                    end
+                end 
+            end 
+        end 
+    end 
+    %% Finding the indexes of maximum value. 
+%    [maxv, pos] = max(acc(:));
+%    [aidx, bidx, cidx, didx, eidx] = ind2sub(size(acc), pos);
+%    a = var_vals(aidx);
+%    b = var_vals(bidx);
+%    c = var_vals(cidx);
+%    d = var_vals(didx);
+%    e = var_vals(eidx);
 
 
