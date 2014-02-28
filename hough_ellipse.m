6c782f6da76f5feda42453cf5160f3ebdbf86193
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-02-28 12:31:44 -0300

    First implementation of an efficient ellipse detector
    * Look the reference [1]
    * FIXME: There is a division by zero problem.

--- hough_ellipse.m
+++ hough_ellipse.m
@@ -6,72 +6,82 @@
 # ellipses.
 # 
 # Reference: 
-# http://en.wikipedia.org/wiki/Hough_transform
-# http://en.wikipedia.org/wiki/Randomized_Hough_transform
+# [1] Y. Xie and Q. Ji, “A new efficient ellipse detection method,” Pattern
+# Recognition, 2002. Proceedings. 16th …, vol. 2, pp. 957–960, 2002.
+#
+# [2] http://en.wikipedia.org/wiki/Hough_transform
+# [3] http://en.wikipedia.org/wiki/Randomized_Hough_transform
 # ========================================================================================= 
 #
 # Using the following general equation to define an ellipse:
 # ax^2 + by^2 + cx + dy + e = 0
 
 
-function [a, b, c, d, e] = hough_ellipse(img)
+function [parameters] = hough_ellipse(img)
 
+    MIN_MAJOR_DISTANCE = 10;
+    MIN_VOTES = 5;
     [width height] = size(img);
-    %%% Defining the possible values for each variable of the ellipse equation.
-    %% Function to generate the sequence of values for a variable, where v is a
-    %% vector with all not quantized values.
-    vals_generator = @(fn, v) unique(fn(v));
-    %% Function to quantize a value by a factor.
-    qfac = 16;
-    quantization = @(x) floor(x/qfac)*qfac;
-    max_val = max([width height]);
-    var_vals = vals_generator(quantization, [-max_val:max_val]);
-
-    vars_len = length(var_vals);
-    acc = uint32(zeros(vars_len, vars_len, vars_len, vars_len, vars_len));
-    %% Loop over all nonzero pixels of the image.
+    %% Finding all nonzero pixels of the image, possible ellipse's pixels.
     [xs ys] = find(img);
-    var_idxs = 1:vars_len;
-    for xyidx = 1:length(xs)
-        x = xs(xyidx);
-        y = ys(xyidx);
-        for aidx = var_idxs
-            for bidx = var_idxs
-                for cidx = var_idxs
-                    for didx = var_idxs
-                        a = var_vals(aidx);
-                        b = var_vals(bidx);
-                        c = var_vals(cidx);
-                        d = var_vals(didx);
-                        e = -(a*x^2 + b*y^2 + c*x + d*y);
-                        e_quant = quantization(e);
-                        %% Calculating the array index of the value e_quant.
-                        %% Otherwise, we would need to linearly search for the
-                        %% index in the array [-max_val:max_val].
-                        zero_idx = ceil(vars_len/2);
-                        rel_idx = e_quant/qfac;
-                        if e_quant > 0
-                            eidx = zero_idx + rel_idx;
-                        else 
-                            eidx = zero_idx - rel_idx;
-                        end 
-                        %% Incrementing the score of the corresponding ellipse
-                        %% variables.
-                        if eidx <= vars_len
-                            acc(aidx, bidx, cidx, didx, eidx)++;
-                        end
-                    end
-                end 
-            end 
-        end 
-    end 
-    %% Finding the indexes of maximum value. 
-%    [maxv, pos] = max(acc(:));
-%    [aidx, bidx, cidx, didx, eidx] = ind2sub(size(acc), pos);
-%    a = var_vals(aidx);
-%    b = var_vals(bidx);
-%    c = var_vals(cidx);
-%    d = var_vals(didx);
-%    e = var_vals(eidx);
-
+    pixels = [xs ys];
+    %% Function to calculate the Euclidean distance between two points.
+    distance = @(p1, p2) sqrt((p1(1) - p2(1))^2 + (p1(2) - p2(2))^2);
+    %% ij1, ij2 are indexes of (x1, y1) and (x2, y2), following the reference [1].
+    for ij1 = 1:(length(xs)-1)
+        for ij2 = (ij1+1):length(xs)
+            x1 = pixels(ij1, 1);
+            y1 = pixels(ij1, 2);
+            x2 = pixels(ij2, 1);
+            y2 = pixels(ij2, 2);
+            d12 = distance([x1 y1], [x2 y2]);
+            %% Accumulator for the minor axis' half-length.
+            acc = sparse(1, max(width, height)/2);
+            if  d12 > MIN_MAJOR_DISTANCE
+                %% Center
+                x0 = (x1 + x2)/2;
+                y0 = (y1 + y2)/2;
+                %% Half-length of the major axis.
+                a = d12/2;
+                %% Orientation.
+                alpha = atan((y2 - y1)/(x2 - x1));
+                %% Distances between the two points and the center.
+                d01 = distance([x1 y1], [x0 y0]);
+                d02 = distance([x2 y2], [x0 y0]);
+                for ij3 = 1:length(xs)
+                    %% The third point must be a different point, obviously.
+                    if (ij3 != ij1) && (ij3 != ij2)
+                        continue;
+                    endif 
+                    x3 = pixels(ij3, 1);
+                    y3 = pixels(ij3, 2);
+                    d03 = distance([x3 y3], [x0 y0]);
+                    %% Distance f
+                    if  d03 < d01
+                        f = distance([x3 y3], [x1 y1]);
+                    elseif d03 < d02
+                        f = distance([x3 y3], [x2 y2]);
+                    else 
+                        continue;
+                    endif
+                    %% Estimating the half-length of the minor axis.
+                    cos2_tau = ((a^2 + d03^2 - f^2) / (2 * a * d03))^2;
+                    sin2_tau = 1 - cos2_tau;
+                    b = round(sqrt((a^2 * d03^2 * sin2_tau) /...
+                                (a^2 * (-d03^2) * cos2_tau)));
+                    %% Changing the score of the accumulator. 
+                    acc(b)++;
+                endfor
+                %% Taking the highest score.
+                [sv si] = max(acc);
+                if sv > MIN_VOTES 
+                    %% Ellipse detected!
+                    parameters = [x0 y0 a b alpha];
+                    return;
+                endif
+            endif
+        endfor
+    endfor
+    printf("No ellipses detected!\n");
+endfunction
 
