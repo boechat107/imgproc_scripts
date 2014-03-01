9ca14b1242c809006290d9691fe49b0c127ed209
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-03-01 17:19:34 -0300

    Detection of one ellipse is working
    * The distance fn was replaced by norm().
    * The order of the index ij2 was inverted hoping that
    we would take the farthest pixels first. Another way
    could be taking a random number.
    * The accumulator was replaced by a normal array, which
    can be reset without needing to free and allocate memory.
    * Fixed the calculation of "f" and "b".

--- hough_ellipse.m
+++ hough_ellipse.m
@@ -13,31 +13,28 @@
 # [3] http://en.wikipedia.org/wiki/Randomized_Hough_transform
 # ========================================================================================= 
 #
-# Using the following general equation to define an ellipse:
-# ax^2 + by^2 + cx + dy + e = 0
 
 
 function [parameters] = hough_ellipse(img)
 
     MIN_MAJOR_DISTANCE = 10;
-    MIN_VOTES = 5;
+    MIN_VOTES = 10;
     [width height] = size(img);
     %% Finding all nonzero pixels of the image, possible ellipse's pixels.
-    [xs ys] = find(img);
+    [ys xs] = find(img);
     pixels = [xs ys];
-    %% Function to calculate the Euclidean distance between two points.
-    distance = @(p1, p2) sqrt((p1(1) - p2(1))^2 + (p1(2) - p2(2))^2);
+    %% Accumulator for the minor axis' half-length.
+    acc = zeros(1, max(width, height)/2);
     %% ij1, ij2 are indexes of (x1, y1) and (x2, y2), following the reference [1].
     for ij1 = 1:(length(xs)-1)
-        for ij2 = (ij1+1):length(xs)
+        for ij2 = length(xs):-1:(ij1+1)
             x1 = pixels(ij1, 1);
             y1 = pixels(ij1, 2);
             x2 = pixels(ij2, 1);
             y2 = pixels(ij2, 2);
-            d12 = distance([x1 y1], [x2 y2]);
-            %% Accumulator for the minor axis' half-length.
-            acc = sparse(1, max(width, height)/2);
-            if  d12 > MIN_MAJOR_DISTANCE
+            d12 = norm([x1 y1] - [x2 y2]);
+            acc = acc * 0;
+            if  x1 - x2 && d12 > MIN_MAJOR_DISTANCE
                 %% Center
                 x0 = (x1 + x2)/2;
                 y0 = (y1 + y2)/2;
@@ -46,31 +43,30 @@ function [parameters] = hough_ellipse(img)
                 %% Orientation.
                 alpha = atan((y2 - y1)/(x2 - x1));
                 %% Distances between the two points and the center.
-                d01 = distance([x1 y1], [x0 y0]);
-                d02 = distance([x2 y2], [x0 y0]);
+                d01 = norm([x1 y1] - [x0 y0]);
+                d02 = norm([x2 y2] - [x0 y0]);
                 for ij3 = 1:length(xs)
                     %% The third point must be a different point, obviously.
-                    if (ij3 != ij1) && (ij3 != ij2)
+                    if (ij3 == ij1) && (ij3 == ij2)
                         continue;
                     endif 
                     x3 = pixels(ij3, 1);
                     y3 = pixels(ij3, 2);
-                    d03 = distance([x3 y3], [x0 y0]);
+                    d03 = norm([x3 y3] - [x0 y0]);
                     %% Distance f
-                    if  d03 < d01
-                        f = distance([x3 y3], [x1 y1]);
-                    elseif d03 < d02
-                        f = distance([x3 y3], [x2 y2]);
-                    else 
+                    if  d03 >= a
                         continue;
                     endif
+                    f = norm([x3 y3] - [x2 y2]);
                     %% Estimating the half-length of the minor axis.
                     cos2_tau = ((a^2 + d03^2 - f^2) / (2 * a * d03))^2;
                     sin2_tau = 1 - cos2_tau;
                     b = round(sqrt((a^2 * d03^2 * sin2_tau) /...
-                                (a^2 * (-d03^2) * cos2_tau)));
+                                (a^2 - d03^2 * cos2_tau)));
                     %% Changing the score of the accumulator. 
-                    acc(b)++;
+                    if b > 0
+                        acc(b)++;
+                    endif
                 endfor
                 %% Taking the highest score.
                 [sv si] = max(acc);
